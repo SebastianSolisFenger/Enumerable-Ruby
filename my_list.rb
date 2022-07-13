b88a5a3b724b8dc8360316421eadc2f5b3410982
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-07-13 17:38:23 -0300

    Add my_enumerable.rb method and functionalities. Then it was required in the class => MyList

--- my_list.rb
+++ my_list.rb
@@ -1,3 +1,6 @@
+$LOAD_PATH << '.'
+require_relative 'my_enumerable'
+
 class MyList
   include MyEnumerable
 
@@ -15,12 +18,3 @@ list = MyList.new(1, 2, 3, 4)
 
 # Test #all?
 puts(list.all? { |e| e < 5 })
-puts(list.all? { |e| e > 5 })
-
-# Test #any?
-puts(list.any? { |e| e == 2 })
-puts(list.any? { |e| e == 5 })
-
-# Test filter
-puts(list.filter(&:even?))
-puts(list.filter(&:odd?))
