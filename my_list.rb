be6f1129f69921dd8a5e204d67a0e21cdb2802b0
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-07-13 17:17:22 -0300

    create my_list

---
+++ my_list.rb
@@ -0,0 +1,26 @@
+class MyList
+  include MyEnumerable
+
+  def initialize(*list)
+    @list = list
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
+end
+
+# Create our list
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
+
+# Test #any?
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+
+# Test filter
+puts(list.filter(&:even?))
+puts(list.filter(&:odd?))
