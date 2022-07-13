e958bb04ef92284cbcc9b4e6b23f997f3d09c805
Juan Rosario <jrosario19@hotmail.com>
2022-07-13 17:05:43 -0400

    Add some tests cases to the my_list.rb

--- my_list.rb
+++ my_list.rb
@@ -18,3 +18,11 @@ list = MyList.new(1, 2, 3, 4)
 
 # Test #all?
 puts(list.all? { |e| e < 5 })
+p list.all? { |e| e > 5 }
+
+# Test #any?
+puts list.any? { |e| e == 2 }
+p list.any? { |e| e == 5 }
+
+# Test filter method
+p list.filter { |e| e.even? }
